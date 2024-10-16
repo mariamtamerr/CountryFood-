2cb405adf5f4c2d6f2e3af1426aa712670006460
mariamtamerr <memariam5@outlook.com>
2024-10-16 14:54:07 +0300

    fonts

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,20 +3,28 @@
     <!-- ========== FOOTER ========== -->
     <!-- <footer dir="ltr" class="w-full  max-w-2xl mx-auto px-4 sm:px-6 lg:px-8">
   <div class="py-6 border-t border-gray-200  d:border-neutral-700"> -->
-    <div
+    <!-- <div
       dir="ltr"
       class="flex justify-between max-w-[85rem] lg:mt-10 px-4 py-10 lg:px-8 lg:py- mx-auto"
+    > -->
+
+    <div
+      dir="ltr"
+      class="sm:flex  justify-between max-w-[85rem] lg:mt-10 px-4 py-10 lg:px-8 lg:py- mx-auto"
     >
+    <div class="sm:hidden block">
+        <img class="mx-auto sm:mx-0" src="../assets/images/footerlogo.png" />
+      </div>
       <div class="mt-5">
-        <p class="text-sm text-gray-600 d:text-neutral-400">
+        <p class="text-sm text-center text-gray-600 d:text-neutral-400">
           الحقوق محفوظة ©2024 كانتري فود
         </p>
       </div>
       <!-- End Col -->
 
       <!-- Logo -->
-      <div class="">
-        <img src="../assets/images/footerlogo.png" />
+      <div class="sm:block hidden">
+        <img class="mx-auto" src="../assets/images/footerlogo.png" />
       </div>
     </div>
     <!-- </div>
